d7d147275dc7c3d04540cf3343a33ee61cc2744c
xencodes <xencodes@icloud.com>
2021-06-02 10:16:52 +0530

    updated frontpage

--- src/components/Head.js
+++ src/components/Head.js
@@ -9,7 +9,7 @@ export default () => {
   const { colorMode, setColorMode } = useColorMode();
   return (
     <Box>
-      <Image mb={4} src={`/assets/logo-${colorMode}.png`} w={12} />
+      <Image mb={4} src={`/assets/logo-light.png`} w={12} />
       <Flex alignItems="center">
         <Heading>XenCodes</Heading>
         <Spacer />
@@ -28,17 +28,23 @@ export default () => {
           </HStack>
         </Flex>
       </Flex>
-      <Text>Passionate Developer.</Text>
+      <Text>Passionate Developer</Text>
 
-      <Stack spacing={8} mt={6}>
+      <Stack spacing={8} mt={4}>
         <Text>
-          Esse voluptate sit aute ipsum ullamco nulla consectetur veniam aute
-          laborum. Minim consectetur culpa quis esse adipisicing id ullamco
-          nulla. Non voluptate voluptate minim est incididunt eu eu dolor fugiat
-          do. Occaecat ut labore proident ipsum culpa. Veniam consectetur enim
-          ea nisi cillum nisi ad ea et. Do amet deserunt commodo officia
-          exercitation enim. Laborum consequat sint anim ullamco sunt aliqua
-          amet ullamco qui fugiat tempor.
+          I've been passionate about everything that relates to something called
+          CPU since I was a kid. I love to know deeper inside of a processing
+          unit, how it works, and I've always imagined what goes on beneath
+          these tiny things. But that is something that's always been a mystry
+          to me.
+        </Text>
+        <Text>
+          But now, I've now become something called a developer, I've built
+          several programs or so called softwares and tools, and developed
+          dozens of websites. But the most I love to enjoy working on is my
+          favorite "web development". I'm both front and back -end developer,
+          with experience in React, Express, Nest, Mongo and more. And I've a
+          love-hate relationship with JavaScript.
         </Text>
       </Stack>
     </Box>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -35,34 +35,10 @@ export default () => {
       <Heading mb={4}>Recent Posts</Heading>
       <Stack spacing={4} _hover={{ cursor: "pointer" }}>
         <Post
-          title=" Commodo excepteur nostrud labore id qui non."
-          description="Consectetur labore Lorem tempor ex do commodo ad qui est elit sit
-          aliquip sit. Quis officia exercitation eiusmod velit amet fugiat
-          laborum et. Consectetur labore Lorem tempor ex do commodo ad qui est
-          elit sit aliquip sit. Quis officia exercitation eiusmod velit amet
-          fugiat laborum et."
-          image="https://images.unsplash.com/photo-1559705421-4ae9bf6fabb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          tags={["node.js", "js", "webdev"]}
-        />
-        <Post
-          title=" Commodo excepteur nostrud labore id qui non."
-          description="Consectetur labore Lorem tempor ex do commodo ad qui est elit sit
-          aliquip sit. Quis officia exercitation eiusmod velit amet fugiat
-          laborum et. Consectetur labore Lorem tempor ex do commodo ad qui est
-          elit sit aliquip sit. Quis officia exercitation eiusmod velit amet
-          fugiat laborum et."
-          image="https://images.unsplash.com/photo-1559705421-4ae9bf6fabb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          tags={["django", "py", "python"]}
-        />
-        <Post
-          title=" Commodo excepteur nostrud labore id qui non."
-          description="Consectetur labore Lorem tempor ex do commodo ad qui est elit sit
-          aliquip sit. Quis officia exercitation eiusmod velit amet fugiat
-          laborum et. Consectetur labore Lorem tempor ex do commodo ad qui est
-          elit sit aliquip sit. Quis officia exercitation eiusmod velit amet
-          fugiat laborum et."
-          image="https://images.unsplash.com/photo-1559705421-4ae9bf6fabb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          tags={["ruby", "react", "java"]}
+          title="The Dark Side of Web Development"
+          description="A complete web development series covering all aspects of the development process."
+          image="/assets/dark-logo.png"
+          tags={["html", "css", "js", "node.js"]}
         />
       </Stack>
     </Box>
